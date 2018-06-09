278b26741d68e4c4aaffd4eb62fc108f2bf341a2
paulmillen <paul.millen@theappbusiness.com>
2018-06-09 16:44:51 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Hi Nora</h1>
+        </header>
+        <p className="App-intro">
+          What would you like your site to look like?
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
